23ccf94d2f235cb4957d987de0831857b03b3ca0
MaryAnzh <MaryAnzh@mail.ru>
2022-07-01 23:41:27 +0400

    feat: add event loop exsample code

--- src/app/core/pages/js-pages/event-loop/event-loop.component.ts
+++ src/app/core/pages/js-pages/event-loop/event-loop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-event-loop',
@@ -11,64 +12,59 @@ export class EventLoopComponent implements OnInit {
   `<button (click)="task01()">Show</button>`,
   ]
 
-  public allStringCount: number = 5;
+  public allStringCount: number = 22;
 
   public stringSelectIterationArr: boolean[] = new Array(2).fill(false);
 
   public selectStringNumber: number = -1;
 
-  constructor() { }
+  constructor(
+    private _viewportScroller: ViewportScroller) { }
 
   ngOnInit(): void {
   }
 
   task01(): void {
-    setTimeout(() => console.log('01. setTimeout 1. контекст макро-задачи-2'), 0);
-    setTimeout(() => console.log('02. setTimeout 2. контекст макро-задачи-3'), 0);
-    setTimeout(() => console.log('03. setTimeout 3.контекст макро-задачи-4'), 0);
+    setTimeout(() => console.log('01. Макро-задача-2, setTimeout-1'), 0);
 
-    console.log('04. log контекст Макро-задачи-1');
+    console.log('02. Макро-задача-1, log');
 
     const a = new Promise((resolve, reject) => {
-      console.log('05, Promise, контекст Макро-задачи-1');
       setTimeout(() => {
-        console.log('06. setTimeout in Promise, log, контекст Макро-задачи-5');
+        console.log('03. Макро-задача-3, setTimeout-2 in Promise, log');
         resolve('resolve a');
       }, 0);
     });
-    console.log('08. log контекст Макро-задачи-1');
-    a.then((resolve) => console.log(`09. then ${resolve}, микро-залача-01 к Макро-задаче-5`));
+    console.log('04. Макро-задача-1, log');
+    a.then((resolve) => console.log(`05. Микро-задача-01, then ${resolve}, ждет выполнения  Макро-задачи-3`));
 
-    setTimeout(() => console.log('10. setTimeout 4. контекст макро-задачи-6'), 0);
-
+    setTimeout(() => console.log('06.  Макро-задача-4, setTimeout-3'), 0);
 
     function fun01() {
-      console.log('11. log вложенная фукция fun01, контекст Макро-задачи-1');
-      a.finally(() => console.log('12. finally, микро-задача-02 к Макро-задаче-5'));
+      console.log('07. Макро-задача-1, log вложенная фукция fun01');
+
       function fun02() {
-        setTimeout(() => console.log('13. вложенная функция fun02, контекст Макро-задачи-7'), 0);
-        console.log('14. log вложенная функция fun02, контекст Макро-задачи-1');
-        a.then((resolve) => console.log(`15. a.then, ${resolve} микро-задача-03 к Макро-задаче-5`));
+        setTimeout(() => console.log('08.  Макро-задача-5, setTimeout-4, вложенная функция fun02'), 0);
+        console.log('09. Макро-задача-1, log, вложенная функция fun02');
+        a.then((resolve) => console.log(`10. Макро-задача-3, ${resolve}, ждет выполнения Макро-задачи-3`));
       }
       fun02();
     }
-
     fun01();
 
-    a.finally(() => console.log('16. finally,  микро-задача-04 к Макро-задаче-5'));
-
     const b = new Promise((resolve, reject) => {
-      console.log('17, Promise, контекст Макро-задачи-1');
+      console.log('11, Макро-задача-1, Promise');
       resolve('resolve b');
     });
 
-    b.then((resolve) => console.log(`17. b.then, микро-задача 01 к Макро-задаче-1`));
-    console.log('18. log -- самый нижний, контекста Макро-задачи-1');
+    b.then((resolve) => console.log(`12. Микро-задача-3 b.then, результат выполнения промиса уже есть в Макро-задаче-1`));
+    console.log('13. Макро-задача-1, последний log');
   }
 
   selectedCodeStrinOnClick() {
     this.selectStringNumber++;
     if (this.selectStringNumber === 0) {
+      this.scrollFn('eventLoopPre');
       this.stringSelectIterationArr[this.selectStringNumber] = true;
     } else if (this.selectStringNumber !== 0 && this.selectStringNumber < this.allStringCount) {
       this.stringSelectIterationArr[this.selectStringNumber - 1] = false;
@@ -77,5 +73,9 @@ export class EventLoopComponent implements OnInit {
       this.stringSelectIterationArr[this.selectStringNumber - 1] = false;
       this.selectStringNumber = -1;
     }
+  }
+
+  scrollFn(anchor: string): void {
+    this._viewportScroller.scrollToAnchor(anchor);
   }
 }
