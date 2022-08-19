b398a0c2522458381c9d6e58f4e203c1b516f6c7
MaryAnzh <MaryAnzh@mail.ru>
2022-08-19 20:58:11 +0400

    feat: add Observable info

--- src/app/angular/pages/observable/observable.component.ts
+++ src/app/angular/pages/observable/observable.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { ILinksData } from 'src/app/share/model/links-list.modet';
 import { asyncScheduler, scheduled, of } from 'rxjs';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-observable',
   templateUrl: './observable.component.html',
-  styleUrls: ['./observable.component.scss']
+  styleUrls: ['./observable.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        transform: 'rotate(-90deg)'
+      })),
+      state('closed', style({
+        transform: 'rotate(0deg)'
+      })),
+      transition('open => closed', [
+        animate('.3s')
+      ]),
+      transition('closed => open', [
+        animate('.3s')
+      ]),
+    ]),
+  ]
 })
 
 export class ObservableComponent implements OnInit {
@@ -21,11 +44,12 @@ export class ObservableComponent implements OnInit {
   ];
 
   public code: string[] = [
-`of(value: null): Observable<null>
+    `of(value: null): Observable<null>
 of(value: undefined): Observable<undefined>
 of(): Observable<never>
+of(value: T): Observable<T>
 of(value: T): Observable<T>`,
-`import { of } from 'rxjs';
+    `import { of } from 'rxjs';
 ...
 of(1, 2, 3)
 .subscribe({
@@ -33,13 +57,17 @@ of(1, 2, 3)
   error: err => console.log('error:', err),
   complete: () => console.log('the end'),
 });`,
-`import { of, asyncScheduler, scheduled } from 'rxjs';
+    `import { of, asyncScheduler, scheduled } from 'rxjs';
 
 // Deprecated approach
 of([1, 2, 3], asyncScheduler).subscribe((x) => console.log(x));
 // suggested approach
 scheduled([1, 2, 3], asyncScheduler).subscribe((x) => console.log(x));`,
   ];
+
+  public showOfInfo: boolean = false;
+  public showFromInfo: boolean = false;
+
 
   constructor() { }
 
