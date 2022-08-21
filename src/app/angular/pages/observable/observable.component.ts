854f3661a46a4cb23c6d966b67ccc11fd2ecfd49
MaryAnzh <MaryAnzh@mail.ru>
2022-08-21 14:35:22 +0400

    feat: add Observable info

--- src/app/angular/pages/observable/observable.component.ts
+++ src/app/angular/pages/observable/observable.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ILinksData } from 'src/app/share/model/links-list.modet';
-import { asyncScheduler, scheduled, of, from, fromEvent, Observable, map } from 'rxjs';
+import { asyncScheduler, scheduled, of, from, fromEvent, Observable, map, Subscribable, Subscription } from 'rxjs';
 import {
   trigger,
   state,
@@ -44,12 +44,15 @@ export class ObservableComponent implements OnInit {
   ];
 
   public code: string[] = [
-    `of(value: null): Observable<null>
+//00
+`of(value: null): Observable<null>
 of(value: undefined): Observable<undefined>
 of(): Observable<never>
 of(value: T): Observable<T>
 of(value: T): Observable<T>`,
-    `import { of } from 'rxjs';
+
+//01 of
+`import { of } from 'rxjs';
 ...
 of(1, 2, 3)
 .subscribe({
@@ -57,49 +60,103 @@ of(1, 2, 3)
   error: err => console.log('error:', err),
   complete: () => console.log('the end'),
 });`,
-    `import { of, asyncScheduler, scheduled } from 'rxjs';
+
+//02 scheduled
+`import { of, asyncScheduler, scheduled } from 'rxjs';
 
 // Deprecated approach
 of([1, 2, 3], asyncScheduler).subscribe((x) => console.log(x));
 // suggested approach
 scheduled([1, 2, 3], asyncScheduler).subscribe((x) => console.log(x));`,
-    `from<T>(input: ObservableInput<T>, scheduler?: SchedulerLike): Observable<T>`,
-    `import { from } from 'rxjs';
+
+//03 from
+`from<T>(input: ObservableInput<T>, scheduler?: SchedulerLike): Observable<T>`,
+
+//04 from
+`import { from } from 'rxjs';
 
 const array = [10, 20, 30];
 const result = from(array);
 
 result.subscribe(x => console.log(x));`,
-    `fromEvent<T>(
+
+//05 fromEvent
+`fromEvent<T>(
   target: any,
   eventName: string,
   options?: EventListenerOptions
   | ((...args: any[]) => T), resultSelector?: (...args: any[])
   => T): Observable<T>`,
-    `import { fromEvent } from 'rxjs';
 
-const clicks = fromEvent(document, 'click');
-clicks.subscribe(() => this.clickCount++ );`
+//06 fromEvent click count
+`import { fromEvent } from 'rxjs';
+...
+  public clickCount$: Observable<number> = of(0);
+...
+  ngOnInit(): void {
+    this.click$ = fromEvent(document, 'click').pipe(
+      map((event) => {
+        const elem = <HTMLElement>event.target;
+        return \`<\${elem.tagName}>\`
+      })
+    );
+  }`,
+//07 html
+`<p>Клик номер:</p>
+<p class="count">{{clickCount$ | async}}</p>`,
+
+//08 fromEvent teg name
+`public click$: Observable<string> = of('');
+...
+ngOnInit(): void {
+  this.click = fromEvent(document, 'click').pipe(
+    map((event) => {
+      const elem = <HTMLElement>event.target;
+      return \`<\${elem.tagName}>]\`
+    })
+  );
+}`,
   ];
 
   public showOfInfo: boolean = false;
   public showFromInfo: boolean = false;
   public showFormEventInfo: boolean = false;
 
-  public clickCount: number = 0;
-  public click: Observable<string> = new Observable();
+  public clickCount$: Observable<number> = of(0);
+  public click$: Observable<string> = of('');
 
   constructor() { }
 
   ngOnInit(): void {
-    this.fromEventExample();
-  this.click = fromEvent(document, 'click').pipe(
-    map((event) => {
-      const elem = <HTMLElement>event.target;
-      console.log(elem.tagName);
-      return `<${elem.tagName}>`
-    })
-  );
+    this.click$ = fromEvent(document, 'click').pipe(
+      map((event) => {
+        const elem = <HTMLElement>event.target;
+        return `<${elem.tagName}>`
+      })
+    );
+
+    let count = 0;
+    this.clickCount$ = fromEvent(document, 'click').pipe(
+      map(() => {
+        return count++
+      })
+    );
+
+    // const obs = new Observable((sub) => {
+    //   sub.next(1);
+
+    //   setTimeout(() => {
+    //     sub.next(3);
+    //     sub.complete();
+    //   }, 2000);
+    //   sub.next(2);
+    // });
+
+    // obs.subscribe({
+    //   next: (vl) => console.log(vl),
+    //   error: (err) => console.log('Error: ', err),
+    //   complete: () => console.log('Completed')
+    // });
   }
 
   ofExample(): void {
@@ -123,8 +180,11 @@ clicks.subscribe(() => this.clickCount++ );`
   }
 
   fromEventExample(): void {
-    const clicks = fromEvent(document, 'click');
-    clicks.subscribe(() => this.clickCount++);
+    ;
+  }
+
+  ngonDestroy(): void {
+
   }
 
 }
