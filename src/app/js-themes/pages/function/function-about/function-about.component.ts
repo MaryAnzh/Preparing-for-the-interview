9a7c36ec2574d935aabca98dea42ecb141b0f0ca
MaryAnzh <maryang@mail.ru>
2022-09-12 15:30:03 +0400

    feat: template for nav inclining pages

--- src/app/js-themes/pages/function/function-about/function-about.component.ts
+++ src/app/js-themes/pages/function/function-about/function-about.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ILinksData } from 'src/app/share/model/links-list.modet';
 
 @Component({
   selector: 'app-function-about',
   templateUrl: './function-about.component.html',
   styleUrls: ['./function-about.component.scss']
 })
+
 export class FunctionAboutComponent implements OnInit {
+  public linksData: ILinksData[] = [
+    {
+      title: 'Functions, MDN',
+      url: 'https://developer.mozilla.org/ru/docs/Web/JavaScript/Guide/Functions'
+    },
+    {
+      title: 'function-basics, learn.javascript',
+      url: 'https://learn.javascript.ru/function-basics',
+    }
+  ];
 
   constructor() { }
 
